ec597b2076e4466d3bfb0144c6f9398a2b52dbb2
AlexisPortocarrero <56561232+AlexisPortocarrero@users.noreply.github.com>
2021-03-13 18:58:09 -0300

    la clase d

--- Algortimos/src/TP1/Ejercicio3.java
+++ Algortimos/src/TP1/Ejercicio3.java
@@ -1,4 +1,31 @@
 package TP1;
 
-public class Ejercicio3 {
+import java.util.ArrayList;
+
+public class Ejercicio3<T> {
+    EjercicioMerge ejercicioMerge;
+    
+    public T[] merge(T[] a, T[] b, int clength){
+        int i = 0;
+        int j = 0;
+        T[] c = (T[]) new Object[clength];
+        for (int k = 0; k < clength ; k++) {
+          if(i < a.length && j < b.length) {
+              if (ejercicioMerge.condition(a[i], b[j])) {
+                  c[k] = a[i];
+                  i++;
+              } else if (!ejercicioMerge.condition(a[i], b[j])) {
+                  c[k] = b[j];
+                  j++;
+              }
+          }else if(i == a.length){
+              c[k] = b[j];
+              j++;
+          }else{
+              c[k] = a[i];
+              i++;
+          }
+        }
+        return c;
+    }
 }
---
+++ Algortimos/src/TP1/EjercicioMerge.java
@@ -0,0 +1,5 @@
+package TP1;
+
+public interface EjercicioMerge<T> {
+    boolean condition(T T1,T T2);
+}

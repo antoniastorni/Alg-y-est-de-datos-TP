d0c13892303aa403d79ae4e7888e64ab61621651
antoniastorni <antonia.storni@ing.austral.edu.ar>
2021-03-13 16:59:06 -0300

    .

--- Algortimos/src/TP1/Ejercicio1.java
+++ Algortimos/src/TP1/Ejercicio1.java
@@ -4,29 +4,29 @@ public class Ejercicio1 {
 
     // busqueda secuencial en un conjunto de numeros enteros que devuelve
     // la posicion en donde se encuentra el valor a buscar
-    public boolean SeqSearch ( int a[], int k) {
+    public int SeqSearch ( int a[], int k) {
         for (int i = 0; i < a.length; i++)
             if (k == a[i])
-                return true;
-        return false;
+                return i;
+        return -1;
     }
 
     // busqueda binaria en un conjunto ordenado de numeros enteros que devuelve
     //    // la posicion en donde se encuentra el valor a buscar
-    public boolean BinarySearch ( int a[], int k) {
+    public int BinarySearch ( int a[], int k) {
         int min = 0;
         int max = a.length-1;
         int middle = (min+max)/2;
         while(min <= max) {
             if (k == a[middle])
-                return true;
+                return middle;
             else if (k < a [middle] )
                 max = middle-1;
             else
                 min = middle +1;
             middle = (min+max)/2;
         }
-        return false;
+        return -1;
     }
 
 }
